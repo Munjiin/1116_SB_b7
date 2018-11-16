a2939618580da817f85cd376e4ac0aa321558f7c
jiin <answldls9401@hanmail.net>
2018-11-16 14:15:44 +0900

    부트_마이바티즈세팅

---
+++ src/test/java/org/zerock/B7ApplicationTests.java
@@ -0,0 +1,36 @@
+package org.zerock;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.zerock.mapper.TimeMapper;
+
+
+
+import lombok.Setter;
+import lombok.extern.java.Log;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Log
+public class B7ApplicationTests {
+
+	@Setter(onMethod_=@Autowired)
+	private TimeMapper mapper;
+	
+	@Test
+	public void testTime() {
+	log.info(mapper.getTime2());
+	
+	}
+	
+	
+	
+	@Test
+	public void contextLoads() {
+	}
+
+}
